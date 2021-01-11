1979a086d8d7dc03ecf77f0903cfe143fe434fa5
trungbe <trungnd0601@gmail.com>
2021-01-11 11:29:43 +0700

    validate number phone

---
+++ src/NumberPhone.java
@@ -0,0 +1,11 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class NumberPhone {
+    private static final String NUMBER_REGEX = "^[(]\\d{2}[)]-[(]0\\d{9}[)]$";
+    public boolean validate(String regex){
+        Pattern pattern = Pattern.compile( NUMBER_REGEX);
+        Matcher matcher = pattern.matcher(regex);
+        return matcher.matches();
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args) {
+        NumberPhone numberPhone  = new NumberPhone ();
+        System.out.println(numberPhone.validate("(84)-(0978489648)"));
+        System.out.println(numberPhone.validate("(a8)-(22222222)"));
+
+    }
+}
